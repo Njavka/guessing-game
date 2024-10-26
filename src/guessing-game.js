b646c9cd94777c6785118c28cef44b4408592334
Navka <navka@list.ru>
2024-10-26 23:02:50 +0300

    refactor: minor improvements

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,17 +2,15 @@ class GuessingGame {
     constructor() {
         this.minValue = 0;
         this.maxValue = 100;
-        this.currentGuess = null;
     }
 
     setRange(min, max) {
         this.minValue = min;
         this.maxValue = max;
-        this.currentGuess = null;
     }
 
     guess() {
-        this.currentGuess = Math.ceil((this.minValue + this.maxValue) / 2);
+        this.currentGuess = Math.round((this.minValue + this.maxValue) / 2);
         return this.currentGuess;
     }
 
