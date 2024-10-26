cefc9c2c031b85be6660bf1660629e627e7e8f66
Navka <navka@list.ru>
2024-10-26 22:53:00 +0300

    feat: guessing-game problem solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minValue = 0;
+        this.maxValue = 100;
+        this.currentGuess = null;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
+        this.currentGuess = null;
     }
 
     guess() {
-
+        this.currentGuess = Math.ceil((this.minValue + this.maxValue) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.maxValue = this.currentGuess;
     }
 
     greater() {
-
+        this.minValue = this.currentGuess;
     }
 }
 
